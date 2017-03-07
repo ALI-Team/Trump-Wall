d293507025bffe19c7a460b31d3f7bc30137035e
AxelMatstoms <axel.matstoms@gmail.com>
2017-03-07 15:03:43 +0000

    done some shit idk what really

--- js/index.js
+++ js/index.js
@@ -8,35 +8,43 @@ $(() => {
 function createFact(title, upvotes, altFacts) {
     $(".main-content").append(
 	$("<div></div>" , {"class": "fact"}).append(
-	    $("<div></div>", {"class": "fact__left-aligned"}).append(
-		$("<div></div>", {"class": "fact__action-area"}).append(
-		    $("<div></div>", {"class": "fact__action-container"}).append(
-			$("<button></button>", {"class": "star-button"}).append(
-			    $("<i></i>", {"class": "material-icons"}).text("star")
+	    $("<div></div>", {"class": "fact--main-container"}).append(
+		$("<div></div>", {"class": "fact__left-aligned"}).append(
+		    $("<div></div>", {"class": "fact__action-area"}).append(
+			$("<div></div>", {"class": "fact__action-container"}).append(
+			    $("<button></button>", {"class": "star-button"}).append(
+				$("<i></i>", {"class": "material-icons"}).text("star")
+			    )
+			).append(
+			    $("<p></p>", {"class": "md-caption"}).text(upvotes)
+			)
+		    )
+		).append(
+		    $("<div></div>", {"class": "fact__content-container"}).append(
+			$("<div></div>", {"class": "fact__content"}).append(
+			    $("<p></p>", {"class": "md-headline"}).text(title)
 			)
 		    ).append(
-			$("<p></p>", {"class": "md-caption"}).text(upvotes)
+			$("<div></div>", {"class": "fact__info-container"}).append(
+			    $("<p></p>", {"class": "md-tiny-link"}).text(`${altFacts} alternative fact${altFacts == 1 ? "" : "s"}`)
+			)
 		    )
 		)
 	    ).append(
-		$("<div></div>", {"class": "fact__content-container"}).append(
-		    $("<div></div>", {"class": "fact__content"}).append(
-			$("<p></p>", {"class": "md-headline"}).text(title)
-		    )
-		).append(
-		    $("<div></div>", {"class": "fact__info-container"}).append(
-			$("<p></p>", {"class": "md-tiny-link"}).text(`${altFacts} alternative fact${altFacts == 1 ? "" : "s"}`)
-		    )
+		$("<div></div>", {"class": "fact__expand-container"}).append(
+		    $("<button></button>", {"class": "expand-button"}).append(
+			$("<i></i>", {"class": "material-icons"}).text("expand_more")
+		    ).on("click", function(ev) {
+			$(this).toggleClass("expand-button--expanded");
+			$(this).closest(".fact").toggleClass("fact--expanded"
+		    })
 		)
 	    )
 	).append(
-	    $("<div></div>", {"class": "fact__expand-container"}).append(
-		$("<button></button>", {"class": "expand-button"}).append(
-		    $("<i></i>", {"class": "material-icons"}).text("expand_more")
-		).on("click", function(ev) {
-		    $(this).toggleClass("expand-button--expanded");
-		})
+	    $("<div></div>", {"class": "fact--expanded-container"}).append(
+		$("<h1></h1>", {"class": "md-title"}).text("dank memes")
 	    )
 	)
     );
 }
+    
