e088cf8a0c8380540b6469c35619b1d3534979f7
Ivar Matstoms <ivar.matstoms@gmail.com>
2017-03-09 20:26:19 +0000

    showing facts

--- js/facts.js
+++ js/facts.js
@@ -1,7 +1,7 @@
 function publishFact(msg){
   token=Cookies.get('token');
   if (token === undefined){
-    alert("something bad happend, worse then obama getting elected")
+    alert("something bad happend, something worse then obama getting elected")
     return
   }
    $.ajax({
@@ -12,6 +12,32 @@ function publishFact(msg){
       dataType: "json"
    });
 	//$.post("http://tux.ivar.tech:5000/createfact",{"response":"","text":"IMA FUCK A WHITE BITCH","author":"69"})
+}
+function updateFacts(){
+  req={}
+  token=Cookies.get('token');
+  if (token === undefined){
+alert("something bad happend, something worse then Hillary Clinton")
+return
+  }else{
+    req["token"]=token
+  }
+  var getfactsajax=$.ajax({
+     type: "POST",
+     contentType: "application/json",
+     url: 'http://trumpwall.ru:5000/getfacts',
+     data: JSON.stringify(req),
+     dataType: "json"
+  });
+  getfactsajax.done(function( data ) {
+    $(".main-content").empty()
+    i=0
+    while (i<data.length){
+      createFact(data[i]["fact"],-1,-1)
+      i=i+1
+    }
+
+  })
 }
 function openComposeDialog(){
   $("#composedialogcontainer").attr("style","")
@@ -36,8 +62,14 @@ $(function(){
   $("#newButton").click(function(){
     openComposeDialog()
   })
+  $("#updateFacts").click(function(){
+    updateFacts()
+  })
 
 })
+
+
+
 $(function(){
   req={}
   token=Cookies.get('token');
@@ -57,7 +89,7 @@ $(function(){
     if ("token" in data){
       Cookies.set("token",data["token"],{ expires: 31 })
     }
-
+    updateFacts()
   })
 
 })
