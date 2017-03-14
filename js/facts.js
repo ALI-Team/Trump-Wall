8e6ad523ac891eb8f092ac995c083bcb262b7d5d
Luka Jankovic <lukjan1999@gmail.com>
2017-03-14 14:48:28 +0100

    from master

---
+++ js/facts.js
@@ -0,0 +1,111 @@
+function publishFact(msg){
+  token=Cookies.get('token');
+  if (token === undefined){
+    alert("something bad happend, something worse then obama getting elected")
+    return
+  }
+   $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: 'http://trumpwall.ru:5000/createfact',
+      data: JSON.stringify({"response":"","fact":msg,"author":token}),
+      dataType: "json"
+   });
+	//$.post("http://tux.ivar.tech:5000/createfact",{"response":"","text":"IMA FUCK A WHITE BITCH","author":"69"})
+}
+function publishAltFact(msg,id){
+  token=Cookies.get('token');
+  if (token === undefined){
+    alert("something bad happend, something worse then obama getting elected")
+    return
+  }
+   $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: 'http://trumpwall.ru:5000/createfact',
+      data: JSON.stringify({"response":id,"fact":msg,"author":token}),
+      dataType: "json"
+   });
+	//$.post("http://tux.ivar.tech:5000/createfact",{"response":"","text":"IMA FUCK A WHITE BITCH","author":"69"})
+}
+
+function updateFacts(){
+  req={}
+  token=Cookies.get('token');
+  if (token === undefined){
+alert("something bad happend, something worse then Hillary Clinton")
+return
+  }else{
+    req["token"]=token
+  }
+  var getfactsajax=$.ajax({
+     type: "POST",
+     contentType: "application/json",
+     url: 'http://trumpwall.ru:5000/getfacts',
+     data: JSON.stringify(req),
+     dataType: "json"
+  });
+  getfactsajax.done(function( data ) {
+    $(".main-content").empty()
+    i=0
+    while (i<data.length){
+      createFact(data[i]["fact"],data[i]["id"],-1,-1)
+      i=i+1
+    }
+
+  })
+}
+function openComposeDialog(){
+  $("#composedialogcontainer").attr("style","")
+  $("#dialog-bg-tint").attr("style","")
+}
+function closeComposeDialog(){
+  $("#composedialogcontainer").attr("style","display:none;")
+  $("#dialog-bg-tint").attr("style","display:none;")
+}
+
+$(function(){
+  $("#composesubmit").click(function(){
+    publishFact($("#composearea").val())
+    closeComposeDialog()
+    $("#composearea").val("")
+  })
+  $("#composediscard").click(function(){
+    closeComposeDialog()
+    $("#composearea").val("")
+
+  })
+  $("#newButton").click(function(){
+    openComposeDialog()
+  })
+  $("#updateFacts").click(function(){
+    updateFacts()
+  })
+
+})
+
+
+
+$(function(){
+  req={}
+  token=Cookies.get('token');
+  if (token === undefined){
+
+  }else{
+    req["token"]=token
+  }
+  var initajax=$.ajax({
+     type: "POST",
+     contentType: "application/json",
+     url: 'http://trumpwall.ru:5000/init',
+     data: JSON.stringify(req),
+     dataType: "json"
+  });
+  initajax.done(function( data ) {
+    if ("token" in data){
+      Cookies.set("token",data["token"],{ expires: 31 })
+    }
+    updateFacts()
+  })
+
+})
--- js/index.js
+++ js/index.js
@@ -1,5 +1,88 @@
 $(() => {
-    $(".expand-button").on("click", function(ev) {
-	$(this).toggleClass("expand-button--expanded");
-    });
+    //createFact("Mexicans are just jews but their black", 420, 69);
 });
+
+
+function createFact(title, id,upvotes, altFacts) {
+    $(".main-content").append(
+	$("<div></div>" , {"class": "fact","loaded":"false"}).append(
+	    $("<div></div>", {"class": "fact--main-container"}).append(
+		$("<div></div>", {"class": "fact__left-aligned"}).append(
+		    $("<div></div>", {"class": "fact__action-area"}).append(
+			$("<div></div>", {"class": "fact__action-container"}).append(
+			    $("<button></button>", {"class": "star-button"}).append(
+				$("<i></i>", {"class": "material-icons"}).text("star")
+			    )
+			).append(
+			    $("<p></p>", {"class": "md-caption"}).text(upvotes)
+			)
+		    )
+		).append(
+		    $("<div></div>", {"class": "fact__content-container"}).append(
+			$("<div></div>", {"class": "fact__content"}).append(
+			    $("<p></p>", {"class": "md-headline"}).text(title)
+			)
+		    ).append(
+			$("<div></div>", {"class": "fact__info-container"}).append(
+			    $("<p></p>", {"class": "md-tiny-link"}).text(`${altFacts} alternative fact${altFacts == 1 ? "" : "s"}`)
+			)
+		    )
+		)
+	    ).append(
+		$("<div></div>", {"class": "fact__expand-container"}).append(
+		    $("<button></button>", {"class": "expand-button"}).append(
+			$("<i></i>", {"class": "material-icons"}).text("expand_more")
+		    ).on("click", function(ev) {
+          if ($(this).closest(".fact").attr("loaded")=="false"){
+            var altfactcontainer=$("<div>",{"class":"altfactcontainer"})
+            $(this).closest(".fact").children(".fact--expanded-container").append(altfactcontainer)
+            req={}
+            req["factid"]=id
+            var getaltfactsajax=$.ajax({
+               type: "POST",
+               contentType: "application/json",
+               url: 'http://trumpwall.ru:5000/getaltfacts',
+               data: JSON.stringify(req),
+               dataType: "json"
+            });
+            getaltfactsajax.done(function( data ) {
+              console.log(altfactcontainer)
+              var i=0
+              while (i<data.length){
+
+                altfactcontainer.append($("<div>",{"class":"altfactdiv"})
+                  .append($("<i>",{"class":"material-icons"}).text("star"))
+                  .append($("<span>",{"class":"altfactspan"}).text(data[i]["fact"])))
+                i=i+1
+              }
+
+            })
+
+
+            var input=$("<input></input>",{"class":"altfactcomposeinput","placeholder":"WRONG!"})
+            input.keypress(function (e) {
+              if (e.which == 13) {
+                publishAltFact(input.val(),id)
+                input.val("")
+                return false;    //<---- Add this line
+              }
+            });
+            $(this).closest(".fact").children(".fact--expanded-container").append(input)
+
+
+
+
+            $(this).closest(".fact").attr("loaded","true")
+          }
+			$(this).toggleClass("expand-button--expanded");
+			$(this).closest(".fact").toggleClass("fact--expanded");
+		    })
+		)
+	    )
+	).append(
+	    $("<div></div>", {"class": "fact--expanded-container"})
+
+
+	)
+    );
+}
