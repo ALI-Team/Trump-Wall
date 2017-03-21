ccb1fb82e43aab387724ed7a7809b2c2ec29b7e4
Luka Jankovic <lukjan1999@gmail.com>
2017-03-21 14:50:27 +0100

    asd

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+var isMenuOpen = false;
+var isEventAlreadyHandled = false;
+
+$(function() {
+  $("#buttonOptions").click(function() {
+
+    console.log($("#menu").hasClass("md-menu__hidden"));
+
+
+    if ($("#menu").hasClass("md-menu__hidden")) {
+        $(".md-menu__hidden").attr("class", "md-menu__shown");
+	    isMenuOpen = true;
+	isEventAlreadyHandled = true;
+
+    }
+    else {
+      $(".md-menu__shown").attr("class", "md-menu__hidden");
+    }
+  })
+
+	$(window).click(function() {
+		console.log("nigger");
+		if (isMenuOpen && !isEventAlreadyHandled) {
+       			console.log("hiding");
+			$(".md-menu__shown").attr("class", "md-menu__hidden");
+    		}
+		isEventAlreadyHandled = false;
+	})
+})
+
